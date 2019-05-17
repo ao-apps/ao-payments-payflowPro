2e7ef60f14174d44cfc1761c29d7404164423b42
AO Industries, Inc <support@aoindustries.com>
2019-05-17 00:42:48 -0500

    Authorization results now include an optional replacement masked card number.
    This facilitates the handling of card details that are updated by a provider.
    When updated, the new masked card number is persisted.
    The persisted value is then used to correctly represent the stored card to the user.

--- src/main/java/com/aoindustries/creditcards/payflowPro/PayflowPro.java
+++ src/main/java/com/aoindustries/creditcards/payflowPro/PayflowPro.java
@@ -141,12 +141,12 @@ public class PayflowPro implements MerchantServicesProvider {
 	 * Combines the two street address lines into a single String.
 	 */
 	protected static String getStreetAddress(String line1, String line2) {
-		if(line1==null) {
-			if(line2==null) return "";
+		if(line1 == null) {
+			if(line2 == null) return "";
 			return line2.trim();
 		} else {
 			line1=line1.trim();
-			if(line2==null) return line1;
+			if(line2 == null) return line1;
 			line2=line2.trim();
 			return (line1+" "+line2).trim();
 		}
@@ -164,7 +164,7 @@ public class PayflowPro implements MerchantServicesProvider {
 
 			UserInfo userInfo = new UserInfo(
 				user,
-				vendor==null ? "" : vendor,
+				vendor == null ? "" : vendor,
 				partner,
 				password
 			);
@@ -177,76 +177,76 @@ public class PayflowPro implements MerchantServicesProvider {
 			// Invoice
 			Invoice invoice = new Invoice();
 			String ponum = transactionRequest.getPurchaseOrderNumber();
-			if(ponum!=null && ponum.length()>0) invoice.setPoNum(ponum);
+			if(ponum != null && ponum.length() > 0) invoice.setPoNum(ponum);
 			String invnum = transactionRequest.getInvoiceNumber();
-			if(invnum!=null && invnum.length()>0) invoice.setInvNum(invnum);
+			if(invnum != null && invnum.length() > 0) invoice.setInvNum(invnum);
 			String comment1 = transactionRequest.getDescription();
-			if(comment1 != null && comment1.length()>0) invoice.setComment1("Transaction Description: "+comment1);
+			if(comment1 != null && comment1.length() > 0) invoice.setComment1("Transaction Description: "+comment1);
 			String comment2 = creditCard.getComments();
-			if(comment2 != null && comment2.length()>0) invoice.setComment2("Credit Card Comments: "+comment2);
+			if(comment2 != null && comment2.length() > 0) invoice.setComment2("Credit Card Comments: "+comment2);
 			String orderNumber = transactionRequest.getOrderNumber();
-			if(orderNumber!=null && orderNumber.length()>0) invoice.setCustRef(orderNumber);
+			if(orderNumber != null && orderNumber.length() > 0) invoice.setCustRef(orderNumber);
 
 			// Add tax, ship, and duty amounts to invoice
 			BigDecimal amount = transactionRequest.getAmount();
 			BigDecimal taxAmount = transactionRequest.getTaxAmount();
-			if(taxAmount!=null) amount = amount.add(taxAmount);
+			if(taxAmount != null) amount = amount.add(taxAmount);
 			BigDecimal shippingAmount = transactionRequest.getShippingAmount();
-			if(shippingAmount!=null) amount = amount.add(shippingAmount);
+			if(shippingAmount != null) amount = amount.add(shippingAmount);
 			BigDecimal dutyAmount = transactionRequest.getDutyAmount();
-			if(dutyAmount!=null) amount = amount.add(dutyAmount);
+			if(dutyAmount != null) amount = amount.add(dutyAmount);
 			invoice.setAmt(new Currency(amount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
-			if(taxAmount!=null) invoice.setTaxAmt(new Currency(taxAmount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
+			if(taxAmount != null) invoice.setTaxAmt(new Currency(taxAmount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
 			invoice.setTaxExempt(transactionRequest.getTaxExempt() ? "Y" : "N");
-			if(shippingAmount!=null) invoice.setFreightAmt(new Currency(shippingAmount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
-			if(dutyAmount!=null) invoice.setDutyAmt(new Currency(dutyAmount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
+			if(shippingAmount != null) invoice.setFreightAmt(new Currency(shippingAmount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
+			if(dutyAmount != null) invoice.setDutyAmt(new Currency(dutyAmount.doubleValue(), transactionRequest.getCurrency().getCurrencyCode()));
 
 			// BillTo
 			BillTo billTo = new BillTo();
 			billTo.setFirstName(creditCard.getFirstName());
 			billTo.setLastName(creditCard.getLastName());
 			String email = creditCard.getEmail();
-			if(email!=null && email.length()>0) billTo.setEmail(email);
+			if(email != null && email.length() > 0) billTo.setEmail(email);
 			String phone = creditCard.getPhone();
-			if(phone!=null && phone.length()>0) billTo.setPhoneNum(phone);
+			if(phone != null && phone.length() > 0) billTo.setPhoneNum(phone);
 			String street = getStreetAddress(creditCard.getStreetAddress1(), creditCard.getStreetAddress2());
-			if(street.length()>0) billTo.setStreet(street);
+			if(street.length() > 0) billTo.setStreet(street);
 			String city = creditCard.getCity();
-			if(city!=null && city.length()>0) billTo.setCity(city);
+			if(city != null && city.length() > 0) billTo.setCity(city);
 			String state = creditCard.getState();
-			if(state!=null && state.length()>0) {
+			if(state != null && state.length() > 0) {
 				if(state.length()==2) billTo.setState(state);
 				else logger.log(Level.WARNING, "PayflowPro: state is not two-digits, and no automatic conversion has been implemented, not sending STATE", state);
 			}
 			String zip = creditCard.getPostalCode();
-			if(zip!=null && zip.length()>0) billTo.setZip(CreditCard.numbersOnly(zip));
+			if(zip != null && zip.length() > 0) billTo.setZip(CreditCard.numbersOnly(zip));
 			String cardCountryCode = creditCard.getCountryCode();
-			if(cardCountryCode!=null && cardCountryCode.length()>0) billTo.setBillToCountry(cardCountryCode);
+			if(cardCountryCode != null && cardCountryCode.length() > 0) billTo.setBillToCountry(cardCountryCode);
 			String companyName = creditCard.getCompanyName();
-			if(companyName!=null && companyName.length()>0) billTo.setCompanyName(companyName);
+			if(companyName != null && companyName.length() > 0) billTo.setCompanyName(companyName);
 			invoice.setBillTo(billTo);
 
 			// ShipTo
 			ShipTo shipTo = new ShipTo();
 			String shippingFirstName = transactionRequest.getShippingFirstName();
-			if(shippingFirstName!=null && shippingFirstName.length()>0) shipTo.setShipToFirstName(shippingFirstName);
+			if(shippingFirstName != null && shippingFirstName.length() > 0) shipTo.setShipToFirstName(shippingFirstName);
 			String shippingLastName = transactionRequest.getShippingLastName();
-			if(shippingLastName!=null && shippingLastName.length()>0) shipTo.setShipToLastName(shippingLastName);
+			if(shippingLastName != null && shippingLastName.length() > 0) shipTo.setShipToLastName(shippingLastName);
 			String shippingStreetAddress1 = transactionRequest.getShippingStreetAddress1();
-			if(shippingStreetAddress1!=null && shippingStreetAddress1.length()>0) shipTo.setShipToStreet(shippingStreetAddress1);
+			if(shippingStreetAddress1 != null && shippingStreetAddress1.length() > 0) shipTo.setShipToStreet(shippingStreetAddress1);
 			String shippingStreetAddress2 = transactionRequest.getShippingStreetAddress2();
-			if(shippingStreetAddress2!=null && shippingStreetAddress2.length()>0) {
-				if(shippingStreetAddress1!=null && shippingStreetAddress1.length()>0) shipTo.setShipToStreet2(shippingStreetAddress2);
+			if(shippingStreetAddress2 != null && shippingStreetAddress2.length() > 0) {
+				if(shippingStreetAddress1 != null && shippingStreetAddress1.length() > 0) shipTo.setShipToStreet2(shippingStreetAddress2);
 				else shipTo.setShipToStreet(shippingStreetAddress2);
 			}
 			String shippingCity = transactionRequest.getShippingCity();
-			if(shippingCity!=null && shippingCity.length()>0) shipTo.setShipToCity(shippingCity);
+			if(shippingCity != null && shippingCity.length() > 0) shipTo.setShipToCity(shippingCity);
 			String shippingState = transactionRequest.getShippingState();
-			if(shippingState!=null && shippingState.length()>0) shipTo.setShipToState(shippingState);
+			if(shippingState != null && shippingState.length() > 0) shipTo.setShipToState(shippingState);
 			String shippingPostalCode = transactionRequest.getShippingPostalCode();
-			if(shippingPostalCode!=null && shippingPostalCode.length()>0) shipTo.setShipToZip(CreditCard.numbersOnly(shippingPostalCode));
+			if(shippingPostalCode != null && shippingPostalCode.length() > 0) shipTo.setShipToZip(CreditCard.numbersOnly(shippingPostalCode));
 			String shippingCountry = transactionRequest.getShippingCountryCode();
-			if(shippingCountry!=null && shippingCountry.length()>0) {
+			if(shippingCountry != null && shippingCountry.length() > 0) {
 				if("US".equals(shippingCountry)) shipTo.setShipToCountry("840");
 				else System.err.println("WARNING: PayflowPro: shippingCountry unknown, not sending SHIPTOCOUNTRY: "+shippingCountry);
 			}
@@ -255,15 +255,15 @@ public class PayflowPro implements MerchantServicesProvider {
 			// CustomerInfo
 			CustomerInfo customerInfo = new CustomerInfo();
 			String customerIp = transactionRequest.getCustomerIp();
-			if(customerIp!=null && customerIp.length()>0) customerInfo.setCustIP(customerIp);
+			if(customerIp != null && customerIp.length() > 0) customerInfo.setCustIP(customerIp);
 			String customerId = creditCard.getCustomerId();
-			if(customerId!=null && customerId.length()>0) customerInfo.setCustCode(customerId);
+			if(customerId != null && customerId.length() > 0) customerInfo.setCustCode(customerId);
 			invoice.setCustomerInfo(customerInfo);
 
 			// CreditCard
 			paypal.payflow.CreditCard ppCreditCard = new paypal.payflow.CreditCard(creditCard.getCardNumber(), creditCard.getExpirationDateMMYY());
 			String cvv2 = creditCard.getCardCode();
-			if(cvv2!=null && cvv2.length()>0) ppCreditCard.setCvv2(cvv2);
+			if(cvv2 != null && cvv2.length() > 0) ppCreditCard.setCvv2(cvv2);
 			ppCreditCard.setName(CreditCard.getFullName(creditCard.getFirstName(), creditCard.getLastName()));
 			CardTender cardTender = new CardTender(ppCreditCard);
 
@@ -272,7 +272,7 @@ public class PayflowPro implements MerchantServicesProvider {
 
 			// RequestID: Use orderNumber if available, fall-back to PayflowUtility
 			String requestId;
-			if(orderNumber!=null && orderNumber.length()>0) requestId = orderNumber;
+			if(orderNumber != null && orderNumber.length() > 0) requestId = orderNumber;
 			else requestId = PayflowUtility.getRequestId();
 
 			if("A".equals(trxType)) {
@@ -289,7 +289,7 @@ public class PayflowPro implements MerchantServicesProvider {
 				logger.log(Level.WARNING, null, err);
 			}
 			String ss = creditCard.getCustomerTaxId();
-			if(ss!=null && ss.length()>0) addTrimmedParameter(request, "SS", ss, 35);
+			if(ss != null && ss.length() > 0) addTrimmedParameter(request, "SS", ss, 35);
 			 */
 		} catch(Exception err) {
 			return new AuthorizationResult(
@@ -298,6 +298,8 @@ public class PayflowPro implements MerchantServicesProvider {
 				TransactionResult.ErrorCode.UNKNOWN.name(),
 				TransactionResult.ErrorCode.UNKNOWN,
 				err.getMessage(),
+				null,
+				null,
 				null,
 				null,
 				null,
@@ -337,6 +339,8 @@ public class PayflowPro implements MerchantServicesProvider {
 				null,
 				null,
 				null,
+				null,
+				null,
 				null
 			);
 		}
@@ -349,11 +353,11 @@ public class PayflowPro implements MerchantServicesProvider {
 		String respMsg = transactionResponse.getRespMsg();
 		String authCode = transactionResponse.getAuthCode();
 		String avsAddr = transactionResponse.getAvsAddr(); // Y, N, X, or null
-		if(avsAddr==null || avsAddr.length()==0) avsAddr="?";
+		if(avsAddr == null || avsAddr.length()==0) avsAddr="?";
 		String avsZip = transactionResponse.getAvsZip(); // Y, N, X, or null
-		if(avsZip==null || avsZip.length()==0) avsZip="?";
+		if(avsZip == null || avsZip.length()==0) avsZip="?";
 		String iavs = transactionResponse.getIavs(); // International AVS.  Y, N, X, or null
-		if(iavs==null || iavs.length()==0) iavs="?";
+		if(iavs == null || iavs.length()==0) iavs="?";
 		// String cardSecure = transactionResponse.getCardSecure(); // ???
 
 		// Convert to CvvResult
@@ -363,7 +367,7 @@ public class PayflowPro implements MerchantServicesProvider {
 		else if("X".equals(cvv2Match)) cvvResult = AuthorizationResult.CvvResult.NOT_SUPPORTED_BY_ISSUER;
 		else {
 			String cvv2 = creditCard.getCardCode();
-			if(cvv2!=null && cvv2.length()>0) cvvResult = AuthorizationResult.CvvResult.NOT_PROCESSED;
+			if(cvv2 != null && cvv2.length() > 0) cvvResult = AuthorizationResult.CvvResult.NOT_PROCESSED;
 			else cvvResult = AuthorizationResult.CvvResult.CVV2_NOT_PROVIDED_BY_MERCHANT;
 		}
 
@@ -394,6 +398,8 @@ public class PayflowPro implements MerchantServicesProvider {
 				null,
 				null,
 				pnref,
+				null,
+				null,
 				Integer.toString(result),
 				AuthorizationResult.ApprovalResult.APPROVED,
 				null,
@@ -437,6 +443,8 @@ public class PayflowPro implements MerchantServicesProvider {
 				null,
 				null,
 				pnref,
+				null,
+				null,
 				Integer.toString(result),
 				AuthorizationResult.ApprovalResult.DECLINED,
 				respMsg,
@@ -458,6 +466,8 @@ public class PayflowPro implements MerchantServicesProvider {
 				null,
 				null,
 				pnref,
+				null,
+				null,
 				Integer.toString(result),
 				AuthorizationResult.ApprovalResult.HOLD,
 				null,
@@ -610,6 +620,8 @@ public class PayflowPro implements MerchantServicesProvider {
 				errorCode,
 				respMsg,
 				pnref,
+				null,
+				null,
 				null,
 				null,
 				null,
