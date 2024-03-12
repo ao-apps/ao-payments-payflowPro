b97cbcb0aa75ad1e2dfa56068adfcfc78d0c31b5
AO Industries, Inc <support@aoindustries.com>
2024-03-12 18:45:49 -0500

    Fixes for checkstyle-10.14.1

--- src/main/java/com/aoapps/payments/payflowPro/PayflowPro.java
+++ src/main/java/com/aoapps/payments/payflowPro/PayflowPro.java
@@ -1,6 +1,6 @@
 /*
  * ao-payments-payflowPro - Provider for the Payflow Pro XMLPay system.
- * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2019, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -177,13 +177,13 @@ public class PayflowPro implements MerchantServicesProvider {
       SDKProperties.setHostPort(HOST_PORT);
       SDKProperties.setTimeOut(TIMEOUT);
 
-      UserInfo userInfo = new UserInfo(
+      final UserInfo userInfo = new UserInfo(
           user,
           vendor == null ? "" : vendor,
           partner,
           password
       );
-      PayflowConnectionData connectionData = new PayflowConnectionData(
+      final PayflowConnectionData connectionData = new PayflowConnectionData(
           transactionRequest.getTestMode() ? TEST_HOST_ADDRESS : LIVE_HOST_ADDRESS,
           HOST_PORT,
           TIMEOUT
